e45866a615b7129a1b30d7ba2ab0f87d776d373b
Park Sung Min <vamf12@gmail.com>
2016-01-13 17:30:05 +0900

    version 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='paginatify',
-        version='0.0.2',
+        version='0.0.3',
         packages=['paginatify'],
         zip_safe=False,
         install_requires=[]
