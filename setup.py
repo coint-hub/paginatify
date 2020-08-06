4f1990b8bbc371ffd332a43f7a149f729663e324
Park Sung Min <vamf12@gmail.com>
2020-08-06 14:40:08 +0900

    update new style setup.py and Makefile

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-        name='paginatify',
-        version='0.0.3',
-        packages=['paginatify'],
-        zip_safe=False,
-        install_requires=[]
+    name='paginatify',
+    version='0.0.4',
+    packages=['paginatify'],
+    zip_safe=False,
+    install_requires=[]
 )
